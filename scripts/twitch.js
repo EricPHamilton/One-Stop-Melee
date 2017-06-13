da37e42d1406c8a93a6286ed2674d107d809d73d
EricPHamilton <ehamilton13@gmail.com>
2017-06-12 21:30:43 -0400

    Added directories for similar types of files, Also, resolved issue #1 by making the images and links clickable in twitch tab.

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -2,6 +2,13 @@ var client_id = config.CLIENT_ID;
 var verify_api_key_request = 'https://api.twitch.tv/kraken/users/44322889?client_id=' + client_id
 var query_streams_request = 'https://api.twitch.tv/kraken/streams/?game=Super%20Smash%20Bros.%20Melee'
 
+window.addEventListener('click', function(e){
+    console.log(e.target.href);
+    if(e.target.href !== undefined){
+        chrome.tabs.create({url : e.target.href})
+    }
+})
+
 function openURL(url) {
     window.open(url);
 }
@@ -9,21 +16,23 @@ function openURL(url) {
 function addStreamToHTML(stream) {
     var new_node = document.createElement('div');
 
-    var a = document.createElement('a');
+    var link = document.createElement('a');
     var link_text = document.createTextNode(stream.channel.name + '\n');
-    a.append(link_text);
-    a.href = stream.channel.url;
-
-    var img_link = document.createElement('a');
-    img_link.href = stream.channel.url;
+    link.append(link_text);
+    link.href = stream.channel.url;
 
     var img = document.createElement('img');
     img.src = stream.preview.medium;
+    img.href = stream.channel.url;
 
-    img_link.appendChild(img);
+    //img_container is created for the sole reason to have the cursor turn into the 'clickable' status.
+    //The href bound to img_container does nothing.
+    var img_container = document.createElement('a');
+    img_container.href = stream.channel.name;
+    img_container.appendChild(img);
 
-    new_node.appendChild(a);
-    new_node.appendChild(img_link);
+    new_node.appendChild(link);
+    new_node.appendChild(img_container);
 
     var stream_list = document.getElementById("streams");
     stream_list.appendChild(new_node);
